b534fb569eaf686f3373f5043f1c978295696e20
guilherme <guilherme@inceres.com.br>
2020-07-17 23:05:15 -0300

    Core adjusts.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Guilherme de Lima Barros - Software Engineer',
     author: 'None',
-    siteUrl: ''
+    siteUrl: 'https://limaguilherme.github.io'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
