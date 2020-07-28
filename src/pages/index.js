bf26d7462efc2b48b05f1921b0375ddbdd06bb44
guilherme <guilherme@inceres.com.br>
2020-07-28 15:33:39 -0300

    Working on css Adding More social networks Improving Skills page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Link } from "gatsby";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import  { faGithub, faMedium, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import  { faGithub, faMedium, faLinkedin, faStackOverflow, faSteam } from '@fortawesome/free-brands-svg-icons'
 
 import Layout from '../components/Layout';
 import SEO from "../components/SEO";
@@ -18,6 +18,8 @@ const Home = () => (
       <hr />
       <p>
         I'm a software engineer with over 4 years of experience working with web applications.
+      </p>
+      <p>
         You can see some of my <Link to="/skills">skills</Link> here, or you can see some of my
         {' '}
         <Link to="/projects">projects</Link>
@@ -25,29 +27,37 @@ const Home = () => (
         <Link to="/articles">articles</Link>
         {'.'}
       </p>
+      <hr />
+      <div className="has-text-centered">
+        <p>You can find here some of my profiles:</p>
 
-      <div className="notification is-light has-text-centered">
-        <p>If you want to contact me for work purposes or if you have any questions send me an email or add me on social networks</p>
-        <p>Email: guilherme.lb@outlook.com</p>
-        <p>Cellphone: (19) 9 97122-3873</p>
-
-    <div className="has-text-centered">
-      <a href="https://github.com/limaGuilherme" target="_blank" aria-label="github">
+        <div className="has-text-centered ">
+          <a className="social-media-icons" href="https://github.com/limaGuilherme" target="_blank" aria-label="github">
         <span className="icon is-large fa-1x">
           <FontAwesomeIcon icon={faGithub} />
         </span>
-      </a>
-      <a href="https://medium.com/@guilherme.lb" target="_blank" aria-label="medium">
+          </a>
+          <a className="social-media-icons" href="https://medium.com/@guilherme.lb" target="_blank" aria-label="medium">
         <span className="icon is-large fa-1x">
           <FontAwesomeIcon icon={faMedium} />
         </span>
-      </a>
-      <a href="https://www.linkedin.com/in/guilhermelb/" target="_blank" aria-label="linkedin">
+          </a>
+          <a className="social-media-icons" href="https://www.linkedin.com/in/guilhermelb/" target="_blank" aria-label="linkedin">
         <span className="icon is-large fa-1x">
           <FontAwesomeIcon icon={faLinkedin} />
         </span>
-      </a>
-    </div>
+          </a>
+          <a className="social-media-icons" href="https://pt.stackoverflow.com/users/30571/guilherme-lima" target="_blank" aria-label="linkedin">
+        <span className="icon is-large fa-1x">
+          <FontAwesomeIcon icon={faStackOverflow} />
+        </span>
+          </a>
+          <a className="social-media-icons" href="https://steamcommunity.com/profiles/76561198116602624/" target="_blank" aria-label="linkedin">
+        <span className="icon is-large fa-1x">
+          <FontAwesomeIcon icon={faSteam} />
+        </span>
+          </a>
+        </div>
 
       </div>
 
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Layout from 'components/Layout';
 import SEO from 'components/SEO';
-import Card from 'components/Card';
-
 
 const Skills =  () => (
   <Layout>
@@ -19,16 +17,74 @@ const Skills =  () => (
         My specialites include quickly learning new skills and progamming languages, likewise:
       </p>
 
-      <div className="columns is-centered">
-        <div className="column is-half">
-          <div className="content">
-            <ul>
-              <li>Arroz</li>
-              <li>Arroz</li>
-              <li>Arroz</li>
-            </ul>
+      <div className="columns is-multiline">
+        <div className="column is-one-third">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                Major
+              </p>
+              <a className="card-header-icon" href="#"><span className="icon"><i aria-hidden="true" className="fas fa-tools"></i></span></a>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                <div className="tags">
+                  <span className="tag">Python</span>
+                  <span className="tag">Flask</span>
+                  <span className="tag">Django</span>
+                  <span className="tag">ElasticSearch</span>
+                  <span className="tag">OOP</span>
+                  <span className="tag">DDD</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <div className="column is-one-third">
+          <div className="card">
+            <header className="has-text-centered card-header">
+              <p className=" card-header-title ">
+                Sou buraco
+              </p>
+              <a className="card-header-icon" href="#"><span className="icon"><i aria-hidden="true" className="fas fa-tools"></i></span></a>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                <div className="content">
+                  <div className="tags">
+                    <span className="tag">Python</span>
+                    <span className="tag">Flask</span>
+                    <span className="tag">Django</span>
+                    <span className="tag">ElasticSearch</span>
+                    <span className="tag">OOP</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="column is-one-third">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                Soft Skills
+              </p>
+              <a className="card-header-icon" href="#"><span className="icon"><i aria-hidden="true" className="fas fa-tools"></i></span></a>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                <div className="tags">
+                  <span className="tag">Clean Communication</span>
+                  <span className="tag">Problem-Solving</span>
+                  <span className="tag">Work Ethic</span>
+                  <span className="tag">Time Management</span>
+                  <span className="tag">quickly learning</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </section>
